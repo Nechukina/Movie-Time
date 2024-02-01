d60b49077d6aa8dd3524d109a18d09018d698553
Ksenia <ksenechka.93@gmail.com>
2024-02-01 12:31:31 +0300

    fix: backend url

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -11,7 +11,7 @@ const StatusCodeMapping: Record<number, boolean> = {
 
 const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
-const BACKEND_URL = 'https://12.react.pages.academy/wtw';
+const BACKEND_URL = 'https://12.react.htmlacademy.pro/wtw';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
